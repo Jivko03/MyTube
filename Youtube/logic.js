4ae62338a2fe919a6fba63fd28edddab29ad13bb
Jivko03 <47494058+Jivko03@users.noreply.github.com>
2025-10-22 21:45:08 +0300

    Add files via upload

--- Youtube/logic.js
+++ Youtube/logic.js
@@ -2,20 +2,7 @@ var seach = document.getElementById("search-bar");
 var button = document.getElementById("seach-btn");
 
 var inputvalue;
-
-seach.addEventListener('keydown',(event) =>
-    {if (event.key === 'Enter')
-        {
-            inputvalue = seach.value;
-            console.log(inputvalue);
-            inputvalue.replace(/ /g, '+');
-            window.open("https://www.youtube.com/results?search_query="+inputvalue).focus();
-
-        }
-
-    })
-
-button.onclick = function () 
+var searchLogic = function() 
 {
     inputvalue = seach.value;
     console.log(inputvalue);
@@ -24,3 +11,7 @@ button.onclick = function ()
 
 }
 
+seach.addEventListener('keydown',(event) =>
+    {if (event.key === 'Enter'){searchLogic()}})
+
+button.onclick = searchLogic();
